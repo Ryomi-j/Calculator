e5f991eb4acc301be0bf431e011e3194f0e8a130
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-11 14:17:08 +0900

    made frame and added some functions but did not make it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import './styles.css';
 
-function App() {
+const App = () => {
+  const [screenContent, setScreenContent] = useState("0");
+  const [firstNum, setFirstNum] = useState("0");
+  const [secondNum, setSecondNum] = useState("0");
+
+  const onChange1 = (event) => {
+    setFirstNum(event.target.value);
+  };
+  
+  const onChange2 = (event) => {
+    setSecondNum(event.target.value);
+  };
+  
+  const Content = () => {
+
+  };
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="frame">
+        <div className="screen">
+          <p className="screenContent"></p>
+        </div>
+        <div className="keys">
+          <ul className="keyLine1">
+            <li>AC</li>
+            <li className="addMinus">+/-</li>
+            <li>%</li>
+            <li className="operator">÷</li>
+          </ul>
+          <ul className="keyLine2">
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li className="operator">X</li>
+          </ul>
+          <ul className="keyLine3">
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li className="operator">-</li>
+          </ul>
+          <ul className="keyLine4">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li className="operator">+</li>
+          </ul>
+          <ul className="keyLine5">
+            <li>0</li>
+            <li>.</li>
+            <li>=</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
